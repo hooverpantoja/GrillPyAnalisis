4428dc2184d957c5da4f2f0a81e77ae039bac918
Hoo <hoover.pantoja@gmail.com>
2025-10-24 10:29:51 -0500

    Fixed before new refactor

--- maad_gui.py
+++ maad_gui.py
@@ -177,23 +177,16 @@ def save_csv():
         print('No metadata loaded') 
     else:
         filename= p.save + '/' + 'meta_data'
-        df.md.to_csv(filename, sep='\t', header=True, index=False, encoding='utf-8')
+        df.md.to_csv(filename, Esep='\t', header=True, index=False, encoding='utf-8')
         print('File created:')
         print(filename)   
-        
-    if data.csv_data.empty:
-        print('No raw data loaded') 
-    else:
-        filename= p.save + '/' + 'raw_data'
-        data.md.to_csv(filename, sep='\t', header=True, index=False, encoding='utf-8')
-        print('File created:')
-        print(filename)   
+
         
     if data.csv_summary.empty:
         print('No summary of set of recorders loaded') 
     else:
         filename= p.save + '/' + 'summary' 
-        data.csv_summary.to_csv(filename, sep='\t', header=True, index=False, encoding='utf-8')
+        data.csv_summary.to_csv(filename, Esep='\t', header=True, index=False, encoding='utf-8')
         #data.csv_summary.to_excel('summary.xlsx')
         print('File created:')
         print(filename)         
@@ -310,5 +303,5 @@ l= tk.Label(frame_var, text="bin_per"); l.grid(row=8,column=1)
 bp = tk.Entry(frame_var,bd=5,state='disabled'); bp.grid(row=8,column=2); db.insert(0, "0.25")
 
 #################################################################
-
-root.mainloop()
+if __name__ == "__main__":
+    root.mainloop()
